b2886800b959c89de9978eeeec431c9b07e77637
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2020-04-21 17:57:01 -0500

    Added Jump Not Working

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    private bool isJumping;
+
+    public Rigidbody rb;
+    public float jumpForce;
+
+    public float clamp;
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+        }
+        else
+        {
+            isJumping = false;
+        }
+
+        if (isJumping == false)
+        {
+            transform.position = new Vector3(transform.position.x, clamp, transform.position.z);
+        }
+        else
+        {
+            rb.AddForce(Vector3.up * Time.deltaTime * jumpForce);
+        }
+    }
+}
