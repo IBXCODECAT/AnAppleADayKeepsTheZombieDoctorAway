0595fa25b2347f975ce0de5f6aa0791de73163cc
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2020-04-21 17:30:01 -0500

    Added a player look script

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public float speed;
+
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horazontal = Input.GetAxis("Horizontal");
+        gameObject.transform.position += (Vector3.forward * vertical * Time.deltaTime * speed);
+        gameObject.transform.position += (Vector3.right * horazontal * Time.deltaTime * speed);
+    }
+}
