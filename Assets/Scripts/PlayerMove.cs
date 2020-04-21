c7700dfa0d8c94210d95281497fae8bf25dd6a4c
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2020-04-21 17:45:06 -0500

    Fantasy Skybox Data Added

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public float speed;
+
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horazontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(horazontal, 0, vertical);
+        movement = transform.TransformDirection(movement);
+        gameObject.transform.position += movement * Time.deltaTime * speed;
+    }
+}
