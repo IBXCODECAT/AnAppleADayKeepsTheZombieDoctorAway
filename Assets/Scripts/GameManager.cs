0d192310ce9adaefcf0205a660359c2889f73a6c
buttejac000 <61168291+buttejac000@users.noreply.github.com>
2020-04-28 18:34:55 -0500

    Creation of GameManager Script
    Game manager script was created, multiple varriables were created along with an aray, and a spawn point was established.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject AI;
+    public int waveCount = 5;
+    public float waveWait = 15;
+    public int zombieCount = 25;
+    public int wv1Spawn = 5;
+    public int spawnUp = 5;
+    private bool waveEnd = true;
+    public Transform[] spawnPoint;
+
+    private void Update()
+    {
+        if (waveEnd == true)
+        {
+            waveEnd = false;
+            aiSpawn();
+        }
+    }
+
+    void aiSpawn()
+    {
+        Instantiate(AI, spawnPoint[Random.Range(0, spawnPoint.Length)].position, Quaternion.identity);
+    }
+}
