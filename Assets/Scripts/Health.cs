a7e07f64b9c254ef7c67cf432c57027999593c83
snowyayano <60415398+snowyayano@users.noreply.github.com>
2020-05-04 18:14:52 -0500

    Enemy Modify

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -27,7 +27,7 @@ public class Health : MonoBehaviour
 
     public void death()
     {
-
+        Destroy(gameObject);
     }
 
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Enemy")
+        {
+            col.gameObject.GetComponent<Health>().damageCount(100);
+        }
+    }
+}
