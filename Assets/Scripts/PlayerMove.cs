6f7f8dc02bc0449201eaa9c3b7810342f789b53f
hectorborges <38707692+hectorborges@users.noreply.github.com>
2020-05-05 19:06:50 -0500

    added jumping

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public float speed = 7f;
+    public float gravity = 14f;
+    public float jumpForce = 10f;
+    public LayerMask environment;
 
-    public float speed;
+    float verticalVelocity;
+    CharacterController cc;
+
+    private void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
@@ -15,6 +25,25 @@ public class PlayerMove : MonoBehaviour
         Vector3 movement = new Vector3(horazontal, 0, vertical);
         movement = transform.TransformDirection(movement);
         movement.y = 0;
-        gameObject.transform.position += movement * Time.deltaTime * speed;
+
+        if (cc.isGrounded)
+        {
+            Debug.Log("Is Grounded");
+            verticalVelocity = -gravity * Time.deltaTime;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = jumpForce;
+                Debug.Log("Jump!");
+            }
+        }
+        else
+        {
+            Debug.Log("Not grounded!");
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        Vector3 move = new Vector3(movement.x * speed, verticalVelocity, movement.z * speed);
+        cc.Move(move * Time.deltaTime);
     }
 }
