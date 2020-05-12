425bee5905fedb7f120fb66db9e42c95d087601a
hectorborges <38707692+hectorborges@users.noreply.github.com>
2020-05-12 18:03:54 -0500

    added fire

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -10,19 +10,33 @@ public class Fire : MonoBehaviour
     public float damageFrequency;
     public int damageControl;
 
+    bool damaging;
 
     void Update()
+    {
+        if(!damaging)
+        {
+            damaging = true;
+            StartCoroutine(Damage());
+        }
+    }
+
+    IEnumerator Damage()
     {
         Collider[] colliders = Physics.OverlapSphere(transform.position, damageRadius);
         if (colliders.Length > 0)
         {
-            foreach(Collider col in colliders)
+            foreach (Collider col in colliders)
             {
                 if (col.GetComponent<Health>())
                 {
-                    col.GetComponent<Health>().damageCount
+                    col.GetComponent<Health>().damageCount(damageControl, Vector3.up);
+                    Debug.Log("Burning");
                 }
             }
         }
+
+        yield return new WaitForSeconds(damageFrequency);
+        damaging = false;
     }
 }
